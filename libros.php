677bf7c417e8fd99e5c8948db1b5008fcc4553df
jost3 <1427593@senati.pe>
2023-09-05 10:19:25 -0500

    se agrego una funcion de seleccionar filas de una tabla

--- libros.php
+++ libros.php
@@ -23,6 +23,23 @@
         body{
             background: url(img/fond.jpg);
         }
+        .seleccionada {
+        background-color:white; /* Cambia el color de fondo según tus preferencias */
+        opacity: 0.8; /* transpariencia de la tabla al seleccionar*/
+        }
+        .seleccionar {
+        width: 20px; 
+        height: 20px; 
+        }
+        .seleccionar:checked {
+        border: 2px solid #007bff; /* Cambia el color del borde cuando está seleccionada */
+        }
+        .seleccionar {
+        margin-right: 5px; /* Añade espacio a la derecha de la casilla */
+        }
+        .seleccionar {
+        cursor: pointer;
+        }
         </style>
 </head>
 <body>
@@ -53,16 +70,39 @@
             }
     ?> 
             <div class="tabla_libros">
+                <div class="titulos">SELECCIONAR</div>
                     <div class="titulos">ID</div>
                     <div class="titulos">TITULO</div>
                     <div class="titulos">AUTOR</div>
                     <div class="titulos">DESCRIPCION</div>
                     <?php  $resultado= mysqli_query($conex, $consulta);
                     while($row=mysqli_fetch_assoc($resultado)) {?>
+                    <div class="dato"><input type="checkbox" class="seleccionar"></div>
                     <div class="dato"><?php  echo $row["id"];?></div>
                     <div class="dato"><?php  echo $row["titulo"];?></div>
                     <div class="dato"><?php  echo $row["autor"];?></div>
                     <div class="dato"><?php  echo $row["descripcion"];?></div>
                <?php } ?>
+
+               <script>
+                // Obtiene todos los elementos de la clase 'seleccionar'
+                    const checkboxes = document.querySelectorAll('.seleccionar');
+
+                // Agrega un evento click a cada checkbox
+                    checkboxes.forEach(checkbox => {
+                        checkbox.addEventListener('click', () => {
+                            // Obtiene el div contenedor de la fila
+                            const fila = checkbox.parentElement.parentElement;
+
+                            // Si el checkbox está marcado, aplica un estilo de selección
+                            if (checkbox.checked) {
+                                fila.classList.add('seleccionada');
+                            } else {
+                                fila.classList.remove('seleccionada');
+                                
+                            }
+                        });
+                    });
+</script>
 </body>
 </html>
