3a6c5217e3595af51a282bc6646acb7171a55071
jost3 <1427593@senati.pe>
2023-09-13 14:30:46 -0500

    cambiosa generales para un mme

--- prest_nuev.php
+++ prest_nuev.php
@@ -13,7 +13,7 @@
     </style>
 </head>
 <body>
-    <a href="inic.php">
+    <a href="prestamo.php">
         <img class="icono" src="img/hessen icono.png">
     </a>
     <!--estructura de la pagina-->
@@ -24,14 +24,15 @@
 
         <label for="autor1">Autor:</label>
         <input type="text" id="autor1" name="autor1" required>
-        
-        <label for="descripcion">Descripción:</label>
-    <select id="descripcion" name="descripcion" required>
+
+        <label for="descripcion1">Descripción:</label>
+        <select id="descripcion1" name="descripcion1" required>
         <option value="opcion1">Gabinete 1</option>
         <option value="opcion2">Gabinete 2</option>
         <option value="opcion3">Gabinete 3</option>
         <option value="opcion4">Gabinete 4</option>
         <option value="opcion5">Gabinete 5</option>
+    <!-- Agrega más opciones según sea necesario -->
     </select>
 
     <label for="estado">Estado:</label>
