41428bbba1ecc6b24748231838d1bd2ba2049b61
jost3 <1427593@senati.pe>
2023-09-20 09:39:12 -0500

    se movio css

--- prestamo.php
+++ prestamo.php
@@ -8,86 +8,12 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <link rel="stylesheet" href="css/tabla.css">
     <style>
-        /* Estilos CSS para el botón "Mostrar todos" */
-        .mostrar-todos {
-            margin-top: 10px;
-            padding: 5px 10px;
-            background-color: #f0f0f0;
-            border-radius: 5px;
-            text-decoration: none;
-            display: inline-block;
-            position: absolute;
-            right: 100px;
-            top: 20px;
-
-        }
+       
         body{
             background: url(img/fond.jpg);
+            font-size: 16px; /* Tamaño de fuente predeterminado */
         }
         /* estilo d ela modal (ventana de advertencia)*/
-        .modal {
-            display: none;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, 0.5);
-            z-index: 1;
-        }
-
-        .modal-content {
-            background-color: #fff;
-            width: 300px;
-            margin: 100px auto;
-            padding: 20px;
-            text-align: center;
-            border-radius: 5px;
-        }
-        /*estilo de la imagen hessen icono*/
-        .icono{
-            width: 100px;
-            height: 130px;
-            margin-left: 12px;
-            margin-top: -90px; /*cambiar valor para ajustar la pocicion*/
-            float: left;
-        }
-        .a1 {
-            background-color:white; /* Cambia el color de fondo a tu preferencia */
-            color: black; /* Cambia el color del texto a tu preferencia */
-            padding: 10px 10px;
-            border-radius: 5px;
-            text-decoration: none;
-            border: none;
-            cursor: pointer;
-        }
-
-        /* Estilo para la clase "a2" */
-        .a2 {
-            background-color: #f57573; /* Cambia el color de fondo a tu preferencia */
-            color: white; /* Cambia el color del texto a tu preferencia */
-            padding: 5px 10px;
-            border-radius: 5px;
-            text-decoration: none;
-            border: none;
-            cursor: pointer;
-        }
-
-        .subtitulos, .informacion {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border: 1px solid #000000; /* Agrega bordes a las celdas */
-            padding: 4px; /* Añade un poco de espacio dentro de las celdas */
-        }
-
-        .subtitulos {
-            font-weight: bold;
-            font-size: 1.5em;
-            background-color: hsl(209, 93%, 53%); /* Cambia el color de fondo de los títulos de columna */
-        }
-
-       
     </style>
     <script>
         // JavaScript para redirigir al botón Atrás
@@ -99,7 +25,6 @@
     </script>
     
 </head>
-<body>
 <body>
     <a href="inic.php">
         <img class="icono" src="img/hessen icono.png">
